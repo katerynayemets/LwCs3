5cdfd1367b200f24b10d9466ce3ea4ab4051a3ca
Екатерина Емец <kateryna.iemets@nure.ua>
2024-11-28 01:53:48 +0200

    abobus

---
+++ Interface1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrarygg3
+{
+    public interface IHouse
+    {
+        /// <summary>
+        /// Розрахувати вартість будівництва будинку.
+        /// </summary>
+        /// <returns>Вартість у грошовому вираженні.</returns>
+        decimal CalculateCost();
+
+        /// <summary>
+        /// Розрахувати час будівництва будинку.
+        /// </summary>
+        /// <returns>Час у днях.</returns>
+        int CalculateConstructionTime();
+
+        /// <summary>
+        /// Отримати площу будинку.
+        /// </summary>
+        /// <returns>Площа у квадратних метрах.</returns>
+        double GetArea();
+    }
+
+}
