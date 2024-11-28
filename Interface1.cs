0d98b3ccca3c1aaaf3023ec8b6d29333cdcb6680
Екатерина Емец <kateryna.iemets@nure.ua>
2024-11-28 02:06:41 +0200

    abobus 122

--- Interface1.cs
+++ Interface1.cs
@@ -8,22 +8,13 @@ namespace ClassLibrarygg3
 {
     public interface IHouse
     {
-        /// <summary>
-        /// Розрахувати вартість будівництва будинку.
-        /// </summary>
-        /// <returns>Вартість у грошовому вираженні.</returns>
+  
         decimal CalculateCost();
 
-        /// <summary>
-        /// Розрахувати час будівництва будинку.
-        /// </summary>
-        /// <returns>Час у днях.</returns>
+      
         int CalculateConstructionTime();
 
-        /// <summary>
-        /// Отримати площу будинку.
-        /// </summary>
-        /// <returns>Площа у квадратних метрах.</returns>
+       
         double GetArea();
     }
 
